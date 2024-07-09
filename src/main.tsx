734dd5949cae8ea1f3282a850c9c449e6987a852
diegomated <diegocardwr@gmail.com>
2024-07-09 16:11:12 -0500

    chore: add tilde bar and tauri window config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './tilde-bar.css'
+
+import { appWindow } from '@tauri-apps/api/window'
+document
+  ?.getElementById('titlebar-minimize')
+  ?.addEventListener('click', () => appWindow.minimize())
+document
+  ?.getElementById('titlebar-maximize')
+  ?.addEventListener('click', () => appWindow.toggleMaximize())
+document
+  ?.getElementById('titlebar-close')
+  ?.addEventListener('click', () => appWindow.close())
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
