1bd0a138576755551c2a757a56c45c9addb87794
pruebasGit2 <diegocardwr2@gmail.com>
2024-07-13 11:05:36 -0500

    add updater

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react"
 import { Main } from "./pages/main/main"
+import { Updater } from "./updater";
 
 function App() {
+
+  useEffect(() => {
+    Updater();
+  }, []);
+
   return <Main></Main>
 }
 
